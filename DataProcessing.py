67ef3b91f2e77ed0ab6f1bda3636375f0f0f89bf
HI6Cypher <swhwap.net@gmail.com>
2023-07-22 17:39:56 +0430

    API_HI6_7

---
+++ DataProcessing.py
@@ -0,0 +1,195 @@
+from API_HI6 import DownloadData
+def DataProcessing_currency(currency) :
+    url = "https://api.coinlore.net/api/tickers/"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    def Decision() :
+        for i in data["data"] :
+            if i["name"] == currency.title() :
+                string_1 = "Symbol: %s\nName: %s\nRank: %s\n" \
+                        % (i["symbol"], i["name"], i["rank"])
+                string_2 = "Price USD: %s\nPrice BTC: %s\n" \
+                        % (i["price_usd"], i["price_btc"])
+                string_3 = "Percent Change(24h): %s\nPercent Change(1h): %s\nPercent Change(7d): %s" \
+                        % (i["percent_change_24h"], i["percent_change_1h"], i["percent_change_7d"])
+                return string_1 + string_2 + string_3
+                break
+            else :
+                continue
+        else :
+            return "Unknown Currency"
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return Decision()
+
+def DataProcessing_datatime(keyword) :
+    url = "http://api.codebazan.ir/time-date/?json=all"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    def Decision() :
+        if keyword == "day" :
+            return data["result"]["nameday"]
+        elif keyword == "month" :
+            return data["result"]["month"]
+        elif keyword == "year" :
+            return data["result"]["year"]
+        elif keyword == "time" :
+            return data["result"]["timeen"]
+        elif keyword == "date" :
+            return data["result"]["dateen"]
+        else :
+            return "Use keywords [day, month, year, time, date]"
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return Decision()
+
+def DataProcessing_ipinfo(ip) :
+    url = f"http://ip-api.com/json/{ip}"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    def Decision() :
+        
+        if data["status"] == "success" :
+            string = "Country: %s, %s, %s\nISP: %s\nLatitude: %s\nLongitude: %s" \
+                    % (data["country"], data["regionName"], data["city"], data["isp"], data["lat"],\
+                    data["lon"])
+            return string
+        else :
+            return data["message"]
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return Decision()
+
+def DataProcessing_movieinfo(movie) :
+    url = f"http://www.omdbapi.com/?t={movie}&apikey=4cb67dde"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    
+    def Decision() :
+        if data["Response"] == "True" :
+            key_list = []
+            value_list = ["Title", "Type", "Rated", "Released", "Runtime", "Genre", "Director",
+            "Writer", "Actors", "Country", "Language", "Awards", "imdbRating", "imdbVotes", "imdbID", "Plot"]
+            for i in value_list :
+                key_list.append(data[i])
+            else :
+                string_1 = "Title: %s\nType: %s\nRated: %s\nReleased: %s\nRuntime: %s\nGenre: %s\nDirector: %s\n" \
+                        % (key_list[0], key_list[1], key_list[2], key_list[3], key_list[4], key_list[5], key_list[6])
+                string_2 = "Writer: %s\nActors: %s\nCountry: %s\nLanguage: %s\nAwards: %s\nIMDb Rating: %s\nIMDb Votes: %s\nIMDb ID: %s\nPlot: %s\n" \
+                        % (key_list[7], key_list[8], key_list[9], key_list[10], key_list[11], key_list[12], key_list[13], key_list[14], key_list[15])
+                return string_1 + string_2
+        else :
+            return data["Error"]
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return Decision()
+
+def DataProcessing_myipinfo() :
+    url = "https://get.geojs.io/v1/ip/geo.json"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        string = "Your IP: %s\nCountry: %s, %s\nTimeZone: %s\nLatitude: %s\nLongitude: %s" \
+                % (data["ip"], data["country"], data["region"], data["timezone"], data["latitude"],\
+                data["longitude"])
+        return string
+
+def DataProcessing_passwordgenerator(length) :
+    url = f"http://api.codebazan.ir/password/?length={length}"
+    download = DownloadData(url, "text")
+    data = download.GetData()
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return data
+
+def DataProcessing_translate(fromLan, toLan, text) :
+    url = f"https://api.codebazan.ir/translate/?type=json&from={fromLan}&to={toLan}&text={text}"
+    download = DownloadData(url, "json")
+    data = download.GetData()
+    if data == 503 :
+        return "No connection to Internet!"
+    elif data == 400 :
+        return "Invalid URL!"
+    elif data == 408 :
+        return "Request timed out!"
+    else :
+        return data["result"]
+
+def DataProcessing_weather(lat = None, lon = None, timezone = None) :
+    url_1 = "https://get.geojs.io/v1/ip/geo.json"
+    download_1 = DownloadData(url_1, "json")
+    data_1 = download_1.GetData()
+    if data_1 == 503 :
+        return "No connection to Internet!"
+    elif data_1 == 400 :
+        return "Invalid URL!"
+    elif data_1 == 408 :
+        return "Request timed out!"
+    elif lat and lon and timezone :
+        pass
+    else :
+        lat = data_1["latitude"]
+        lon = data_1["longitude"]
+        timezone = data_1["timezone"]
+    url_2 = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=d7ac3233b2d88a4c8f01032c02eae2db"
+    url_3 = f"https://api.sunrisesunset.io/json?lat={lat}&lng={lon}&timezone={timezone.title()}"
+    download_2 = DownloadData(url_2, "json")
+    data_2 = download_2.GetData()
+    download_3 = DownloadData(url_3, "json")
+    data_3 = download_3.GetData()
+    if data_2 == 503 and data_3 == 503 :
+        return "No connection to Internet!"
+    elif data_2 == 400 and data_3 == 400 :
+        return "Invalid URL!"
+    elif data_2 == 408 and data_3 == 408 :
+        return "Request timed out!"
+    else :
+        city = data_2["name"]
+        weather = data_2["weather"][0]["description"]
+        sunrise = data_3["results"]["sunrise"]
+        sunset = data_3["results"]["sunset"]
+        dawn = data_3["results"]["dawn"]
+        dusk = data_3["results"]["dusk"]
+        daylength = data_3["results"]["day_length"]
+        temp_kelvin = data_2["main"]["temp"]
+        temp_celcius = round(temp_kelvin - 273.15, 2)
+        temp_fahrenheit = round(1.8 * temp_celcius + 32, 2)
+        pressure = data_2["main"]["pressure"]
+        humidity = data_2["main"]["humidity"]
+        wind = data_2["wind"]["speed"]
+        string_1 = "City: %s\nWeather: %s\nTemperature in Kelvin: %s\nTemperature in Fahrenheit: %s\nTemperature in Celcius: %s" \
+                % (city, weather, temp_kelvin, temp_fahrenheit, temp_celcius)
+        string_2 = "\nPressure: %s\nHumidity: %s\nWind: %s Km/h\nSunrise: %s\nSunset: %s\nDawn: %s\nDusk: %s\nDay length: %s" \
+                % (pressure, humidity, wind, sunrise, sunset, dawn, dusk, daylength)
+        return string_1 + string_2
--- Interface.py
+++ Interface.py
@@ -1,5 +1,11 @@
-from DataProcessing_translate import DataProcessing_translate
 from DataClass import DataClass
+from DataProcessing import *
+def Interface_myipinfo() :
+    return DataProcessing_myipinfo()
+
+def Interface_passwordgenerator(length) :
+    return "your password is: %s" % (DataProcessing_passwordgenerator(length))
+
 def Interface_translate(fromLan, toLan, text) :
     if text :
         lans = DataClass.languages
@@ -23,3 +29,13 @@ def Interface_translate(fromLan, toLan, text) :
         return DataProcessing_translate(codename_1, codename_2, text)
     else :
         return "Your text is empty"
+
+def Interface_weather(lat = None, lon = None, timezone = "UTC") :
+    if lat and lon and timezone :
+        if timezone.title() in DataClass.timezones :
+            return DataProcessing_weather(lat, lon, timezone)
+        else :
+            return DataProcessing_weather(lat, lon, timezone = "UTC")
+    else :
+        return DataProcessing_weather()
+
